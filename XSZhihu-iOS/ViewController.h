73808889f8cd08a50d2554066b20db5a61a198f2
BigSong <737393622@qq.com>
2018-03-05 09:05:00 +0800

    第一次提交

---
+++ XSZhihu-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSZhihu-iOS
+//
+//  Created by 薛松 on 2018/3/5.
+//  Copyright © 2018年 bigsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XSZhihu-iOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XSZhihu-iOS
+//
+//  Created by 薛松 on 2018/3/5.
+//  Copyright © 2018年 bigsong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
